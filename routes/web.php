75190ffd09fe0b19f3d321b13e07230399a19084
Ahmad Balya Diwanullatif <ahmad.22095@mhs.unesa.ac.id>
2024-03-19 22:00:27 +0700

    menghapus rtute api.php diganti di web.php lalu menempatkan registercontroller pada auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Rute untuk registrasi dan login
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+// Rute untuk User
+Route::middleware('auth')->group(function () {
+    Route::post('/update-user', [UserController::class, 'update']);
+    Route::get('/get-user', [UserController::class, 'getUser']);
+    Route::post('/logout', [UserController::class, 'logout']);
 });
+
+// Rute untuk Contact
+Route::middleware('auth')->group(function () {
+    Route::post('/create-contact', [ContactController::class, 'create']);
+    Route::post('/update-contact', [ContactController::class, 'update']);
+    Route::get('/get-contact/{id}', [ContactController::class, 'get']);
+    Route::get('/search-contact', [ContactController::class, 'search']);
+    Route::post('/remove-contact', [ContactController::class, 'remove']);
+});
+
+// Rute untuk Address
+Route::middleware('auth')->group(function () {
+    Route::post('/create-address', [AddressController::class, 'create']);
+    Route::post('/update-address', [AddressController::class, 'update']);
+    Route::get('/get-address/{id}', [AddressController::class, 'get']);
+    Route::get('/list-address', [AddressController::class, 'list']);
+    Route::post('/remove-address', [AddressController::class, 'remove']);
+});
+
+// Route untuk halaman landing
+Route::get('/', function () {
+    return view('landing');
+})->name('landing');
