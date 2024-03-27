8ec153b3b919a3d54be763d83e622cb1e98383b4
Ahmad Balya Diwanullatif <ahmad.22095@mhs.unesa.ac.id>
2024-03-28 03:19:32 +0700

    rebuild project (deleting all file)

--- resources/views/landing/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.master')
-@section('container')
-    <h1>Application CRUD</h1>
-    <h2>User, Contact, dan Address</h2>
-    <h3>Dengan data base MySQL</h3>
-    <h4>Melalui Framework Laravel</h4>
-@endsection
